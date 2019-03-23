e8b96ae434fdac3cc72a6caf17ea00d124bf2a33
David Sukiasyan <sdavidk99@icloud.com>
2019-03-24 00:30:40 +0300

    Add logo and Head component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Head from './head/Head.js'
+
+class App extends Component{
+	render(){
+		return(
+			<div>
+				<Head/>
+			</div>
+		);
+	}
+}
+
+export default App
---
+++ src/components/head/Head.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Head extends Component{
+	render(){
+		return(
+			<div>
+				<img src="https://image.freepik.com/free-vector/chef-logo-vector_20448-270.jpg" alt="logo" className="logo"/>
+			</div>
+		);
+	}
+}
+
+export default Head
